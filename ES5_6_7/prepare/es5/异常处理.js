0ce1a09d4005609ef574607744211df8689731f9
gavincoder <907516241@qq.com>
2019-05-23 21:15:35 +0800

    05-23

---
+++ ES5_6_7/prepare/es5/异常处理.js
@@ -0,0 +1,17 @@
+//console.log(WebGLActiveInfo);
+try {
+
+    console.log(gavin)
+
+
+} catch (error) {
+
+//自定义报错内容
+    console.log("程序出错")
+}
+
+console.log("hello js");
+
+
+//好处1:记录日志
+//好处2:不妨碍整体程序运行
