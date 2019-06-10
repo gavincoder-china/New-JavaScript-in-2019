70667b8a619ee80762bfb85c4e7ba1136b3a9f08
gavincoder <907516241@qq.com>
2019-06-10 15:08:13 +0800

    06-10-vue基础

---
+++ Vue板块/VueTest/13_插件/vue-myPlugin.js
@@ -0,0 +1,20 @@
+(function (window) {
+  const MyPlugin = {}
+  MyPlugin.install = function (Vue, options) {
+    // 1. 添加全局方法或属性
+    Vue.myGlobalMethod = function () {
+      console.log('Vue函数对象的myGlobalMethod()')
+    }
+
+    // 2. 添加全局资源
+    Vue.directive('my-directive',function (el, binding) {
+      el.textContent = 'my-directive----'+binding.value
+    })
+
+    // 4. 添加实例方法
+    Vue.prototype.$myMethod = function () {
+      console.log('vm $myMethod()')
+    }
+  }
+  window.MyPlugin = MyPlugin
+})(window)
