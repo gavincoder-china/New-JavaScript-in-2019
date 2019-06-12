0483ed091bce8163b2611e24a7f6e6417c921b92
gavincoder <907516241@qq.com>
2019-06-12 17:11:33 +0800

    06-12-vue写的 Todo日程安排小工具

---
+++ Vue板块/vue_learn/vue02/src/components/TodoHeader.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="todo-header">
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" @keydown.enter="addThing" v-model="content"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoHeader",
+        data() {
+            return {
+                content: ''
+            }
+        },
+        props: {
+            addTodo: Function,
+            required: true
+        },
+        methods: {
+            addThing() {
+
+                let content = this.content.trim();
+                if (!content) {
+                    alert("请输入内容")
+                }else {
+                    const thing = {
+                        content,
+                        IsDown:false
+                    }
+
+
+                    this.addTodo(thing);
+                    this.content = '';
+                }
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-header input {
+        width: 560px;
+        height: 28px;
+        font-size: 14px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 4px 7px;
+    }
+
+    .todo-header input:focus {
+        outline: none;
+        border-color: rgba(82, 168, 236, 0.8);
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+    }
+</style>
