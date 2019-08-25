e96575ae20d4cb49febf2868ac9747490dd6e601
gavincoder <907516241@qq.com>
2019-08-25 16:45:55 +0800

    08-25

--- Vue板块/vue_learn/vue_vuexxdemo002/src/App.vue
+++ Vue板块/vue_learn/vue_vuexxdemo002/src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div>
-    <p>{{$store.state.num}}</p>
+    <p>{{num}}</p>
+    <p>{{numType}}</p>
     <button @click="increase(1)">加</button>
     <button @click="decrease">减</button>
+    <button @click="print">打印</button>
+    <button @click="print02"  value="我是一个测试传值的标签">打印自身的value</button>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions, mapGetters } from "vuex";
 export default {
   name: "app",
+  data() {
+    return {
+      //这个测试一下从state中获取值
+      p1: this.$store.state.pp
+    };
+  },
   computed: {
-    ...mapState(["num"])
+    ...mapState(["num"]),
+    ...mapGetters(["numType"])
   },
 
   methods: {
-    ...mapActions(["increase", "decrease"])
+    ...mapActions(["increase", "decrease"]),
+    print: function() {
+      alert(this.p1);
+    },
+    print02:function(event) {
+      //这边测试把数值传给vuex去管理
+     var str=event.target.value;
+
+     this.$store.dispatch('strUpdate',str)
+    
+    }
   }
 };
 </script>
--- Vue板块/vue_learn/vue_vuexxdemo002/src/store.js
+++ Vue板块/vue_learn/vue_vuexxdemo002/src/store.js
@@ -6,29 +6,38 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     //这边是基本变量
-    num: 99
+    num: 99,
+    pp: 999,
+    str: ''
   },
   mutations: {
     //这边是方法的真正实现
-    INCREASE: function (state,data) {
+    INCREASE: function (state, data) {
       state.num++
-      console.log("我是mutations"+data)
+      console.log("我是mutations" + data)
     },
     DECREASE: function (state) {
       state.num--
+    },
+    strUpdate: function (state, data) {
+      state.str = data;
+      alert(state.str);
     }
 
   },
   actions: {
     //这边是方法的传达
-    increase: function ({ commit },data) {
-      commit("INCREASE",data)
+    increase: function ({ commit }, data) {
+      commit("INCREASE", data)
       console.log(data)
     },
     decrease: function ({ commit }) {
 
 
       commit("DECREASE")
+    },
+    strUpdate: function ({ commit }, data) {
+      commit("strUpdate", data)
     }
 
 
@@ -36,7 +45,9 @@ export default new Vuex.Store({
   },
   getters: {
     //这边是计算属性
-
+    numType: function (state) {
+      return state.num % 2 === 0 ? '偶数' : '奇数'
+    }
 
   }
 })
