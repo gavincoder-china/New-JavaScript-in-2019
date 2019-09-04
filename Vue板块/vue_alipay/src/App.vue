88109711a2cab06affa8a695c94db73493724c16
gavincoder <907516241@qq.com>
2019-09-04 11:08:16 +0800

    09-04-图床源码

---
+++ Vue板块/vue_alipay/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+
+    <form :action="url">
+
+
+      <button type="submit">表单支付</button>
+    </form>
+    <button @click="pay">支付</button>
+   
+  <div v-html="ali">
+
+    
+  </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+export default {
+  name: "app",
+  data() {
+    return {
+      ali: "",
+      url:"http://localhost:8080/alipay/pay"
+    };
+  },
+  methods: {
+    pay: function() {
+      axios({
+        method: "post",
+        url: "http://localhost:8080/alipay/pay"
+      }).then(result => {
+        console.log(result.data);
+         this.ali=result.data
+       // eval(result.data);
+
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
